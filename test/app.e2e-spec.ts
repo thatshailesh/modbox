c831e948e3602dda162b3d0caf71a5cbbe0517e4
Shailesh Shekhawat <sshekhawat356@gmail.com>
2022-06-11 15:23:07 +0530

    test: added e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,69 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('/benford-data (GET) benford stock comparison', () => {
+    return request(app.getHttpServer())
+    .get('/benford-data/PEV')
+    .expect(({body}) => {
+      const expectedResult = [
+        {
+            "leadingDigit": 1,
+            "occurenceCount": 1,
+            "occurencePercent": "33.333 %",
+            "comparisonToBL": "3.233 %"
+        },
+        {
+            "leadingDigit": 2,
+            "occurenceCount": 1,
+            "occurencePercent": "33.333 %",
+            "comparisonToBL": "15.733 %"
+        },
+        {
+            "leadingDigit": 3,
+            "occurenceCount": 0,
+            "occurencePercent": "0.000 %",
+            "comparisonToBL": "-12.500 %"
+        },
+        {
+            "leadingDigit": 4,
+            "occurenceCount": 0,
+            "occurencePercent": "0.000 %",
+            "comparisonToBL": "-9.700 %"
+        },
+        {
+            "leadingDigit": 5,
+            "occurenceCount": 0,
+            "occurencePercent": "0.000 %",
+            "comparisonToBL": "-7.900 %"
+        },
+        {
+            "leadingDigit": 6,
+            "occurenceCount": 1,
+            "occurencePercent": "33.333 %",
+            "comparisonToBL": "26.633 %"
+        },
+        {
+            "leadingDigit": 7,
+            "occurenceCount": 0,
+            "occurencePercent": "0.000 %",
+            "comparisonToBL": "-5.800 %"
+        },
+        {
+            "leadingDigit": 8,
+            "occurenceCount": 0,
+            "occurencePercent": "0.000 %",
+            "comparisonToBL": "-5.100 %"
+        },
+        {
+            "leadingDigit": 9,
+            "occurenceCount": 0,
+            "occurencePercent": "0.000 %",
+            "comparisonToBL": "-4.600 %"
+        }
+      ]
+      expect(body).toEqual(expectedResult)
+    })
+    
+  })
 });
