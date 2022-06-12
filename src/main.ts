2c62199c6218cdce434c576a2ac5b4d687d920bc
Shailesh Shekhawat <sshekhawat356@gmail.com>
2022-06-12 13:26:03 +0530

    fix: read PORT from env for heroku deployment

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //     forbidNonWhitelisted: true,
-  //     transform: true
-  //   })
-  // )
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
